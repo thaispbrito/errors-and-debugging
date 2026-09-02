31bb116abeec2109eb29fbd82da8438a2a5c044f
Thais Palma de Brito <britop.tech@gmail.com>
2026-09-02 11:46:01 -0700

    Fix errors

--- runtime.py
+++ runtime.py
@@ -1,13 +1,17 @@
 def get_3rd_value(my_list):
-    return my_list[2]
+    if len(my_list) >= 3:
+        return my_list[2]
+    return None
 
 def test_get_3rd_value():
     assert get_3rd_value([0, 1, 2]) == 2
     assert get_3rd_value([0, 1]) == None
 
 def get_last_value(my_list):
-    item_count = len(my_list)
-    return my_list[itm_count]
+    item_count = len(my_list) - 1
+    if my_list:
+         return my_list[item_count]
+    return None
 
 def test_get_last_value():
     assert get_last_value([0, 1, 2]) == 2
